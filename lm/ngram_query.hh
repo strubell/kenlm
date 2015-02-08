8b6a070a76dbc6e0688932669180322030c093d9
Emma Strubell <strubell@cs.umass.edu>
2015-02-07 21:37:34 -0500

    cleanup shitcode

--- lm/ngram_query.hh
+++ lm/ngram_query.hh
@@ -121,9 +121,9 @@ template <class Model, class Printer> void Query(const Model &model, bool senten
         } // end for over vocab
 
         while (!probabilityHeap.empty()) {
-            ProbPair p = probabilityHeap.pop();
+            ProbPair p = probabilityHeap.top();
             std::cout << p.probability << std::endl;
-
+            probabilityHeap.pop();
         }
 //    } // end while choosing words
 
